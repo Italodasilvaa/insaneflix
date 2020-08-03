14082975415db6491057c9660470894db46276b2
Italo Wesley <italodasilvaa@gmai.com>
2020-08-03 16:01:08 -0300

    atualizcao codigo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O filme narra a história do jovem Aaron Swartz (1986-2013), um jovem programador norte-americano que acreditava na mudança radical do mundo através da internet e da computação. Durante toda a sua vida, Aaron usou a programação computacional como uma forma de nos ajudar a resolver problemas e tornar o mundo um lugar mais democrático, justo e eficiente. Em uma destas tentativas, Aaron irá usar a rede do MIT (Massachusetts Institute of Technology) para realizar o download massivo de milhões de artigos acadêmicos de uma base de dados privada chamada JSTOR. Nesse meio-tempo, o Ministério Público dos Estados Unidos irá conduzir um processo criminal contra Aaron, que termina por levá-lo ao suicídio. "}
+        videoDescription=
+        {"O melhores videos de comedia os melhores comediantes , pegadinhas e trotes. INSANEFLIX trazendo o melhor conteudo para você se divertir com toda sua familia  "
+      
+      }
+        
       />
 
       <Carousel
